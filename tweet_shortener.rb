a7db45e977513c040971598289104abd3935506a
Tao Xu <nytaoxu@gmail.com>
2019-02-12 20:56:26 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -84,7 +84,7 @@ end
 def shortened_tweet_truncator(tweet)
   shortened_string = selective_tweet_shortener(tweet)
   if shortened_string.length > 140
-    return shortened_string[0..139] + "..."
+    return shortened_string[0..136] + "..."
   else
     return shortened_string
   end
